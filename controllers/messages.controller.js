b407be2e3c3564ec0001e477f6317eb7d2fae988
Harsh Kedia <hkedia@hey.com>
2023-10-30 17:02:27 +0530

    templating engine

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -2,8 +2,11 @@ const path = require('path')
 
 function getMessages(req, res) {
   // res.send('<ul><li>Hello Albert</li></ul>');
-  
-  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skiing.jpg'))
+  res.render('messages', {
+    title: 'Messages to my friends',
+    friend: 'Elon Musk'
+  })
+  // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skiing.jpg'))
 }
 
 function postMessage(req, res) {
